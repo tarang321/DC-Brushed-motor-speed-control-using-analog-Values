3d12714c15cb91996e7d2070a9cdc390e12e91bf
Tarang Srivas <37020491+tarang321@users.noreply.github.com>
2024-08-17 05:02:48 +0530

    Add files via upload

---
+++ Brushed_motor_esc_code/Brushed_motor_esc_code.ino
@@ -0,0 +1,47 @@
+
+//////// DC motor speed control //
+
+
+void setup() {
+    
+}
+
+void loop() {
+    
+    analogWrite(11,00);
+    delay(6000);
+    
+    analogWrite(11,25);
+    delay(4000);
+    
+    analogWrite(11,50);
+    delay(4000);
+    
+    analogWrite(11,75);
+    delay(4000);
+    
+    analogWrite(11,100);
+    delay(4000);
+    
+    analogWrite(11,125);
+    delay(4000);
+    
+    analogWrite(11,150);
+    delay(4000);
+    
+    analogWrite(11,175);
+    delay(4000);
+    
+    analogWrite(11,200);
+    delay(4000);
+    
+    analogWrite(11,225);
+    delay(4000);
+    
+    analogWrite(11,255);
+    delay(7000);
+    
+    analogWrite(11,0);
+    delay(2000);
+    
+}
